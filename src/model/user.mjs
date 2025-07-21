9bc45cb8e90be2733eac9d258ce36ed202a6781d
SSumu <sadeepalsumudu@gmail.com>
2025-07-21 18:35:53 +0530

    MongoDB with Mongoose One-to-One

--- src/model/user.mjs
+++ src/model/user.mjs
@@ -2,10 +2,10 @@
 // 1. First we have to create a schema. Schema is a pattern or flow of architecture. In here, Mongoose uses document. In SQL we about tables. In here, there it talks about documents. So in table in SQL matches to document in ODM(Object Document Mapping) in here.
 // MongoDB is faster than SQL.
 
-import { model, Schema } from "mongoose";
+import { model, Schema, Types } from "mongoose";
 
 // This is the schema. Schema contains objects.
-const userSchema = Schema(
+const userSchema = new Schema( // There the new keyword must be there before the Schema. Because Schema is a class. So an object must be created. So the userSchema is the object.
   {
     // We have to write what we expect put in the collections. Those are objects.
     name: String,
@@ -21,6 +21,14 @@ const userSchema = Schema(
       type: String,
       required: true,
     },
+    // There it must has a property to relate the two schemas. There it must has a property to connect these two schemas. If we want we can give ProfileId directly. But if we make this as the relation exist, there it should give the profile in user's side.
+    profile: {
+      // Options
+      type: Types.ObjectId, // Here the ObjectId means id that automatically comes.
+      ref: "Profile", // ref means to whom this reference has to. This reference has to Profile. This "Profile" name must equal to the name in schema. This "Profile" name must equal to the name "Profile" in profile.mjs which it is that included in "". ( const Profile = model("Profile", profileSchema); ). When user and profile are connected by one-to-one relation, ObjectId must be put to the type with putting ref(reference).
+      // profile must be connected to the user. But id in the profile must be connected. That is why ObjectId is also in here. But profile is not required because user may or may not has a profile. So only the reference is here.
+      unique: true, // Only one profile can be existed for one user.
+    },
   },
   {
     // Options
@@ -32,8 +40,27 @@ const userSchema = Schema(
 // Schema describes how the way model should be created. (like architecture of the model)
 // Then we put the schema into the model. Then it becomes active.
 
-const User = model("User", userSchema); // model(name: typically it is capitalized, schema). From here model(User) is created.
+// There is a One-to-One connection between userSchema and profileSchema.
+// As this is a fact that related to the user, we can make this in here. If we want, we can take this seperately.
+
+// const profileSchema = new Schema({
+//   // We have to write what we expect put in the collections. The data what we expect to pass.
+//   image:{
+//     required:true,
+//     type:String
+//   }
+// },{
+//   timestamps:true // This is for createAt, updateAt
+// }) // There it passes an object inside of it.
+
+// This profileSchema has to be moved to the profile.mjs in model folder as there is a default User in here.
+
+// Now we have to relate these two Schemas.
+
+const User = model("User", userSchema); // model(name: typically it is capitalized, schema). From here model(User) is created. Here the model name is User.
 
 // model is the active part. That means active part in the mongoose is the model.
 
 export default User;
+
+// In here, we have to do this in two ways. But we can do this in one line. But in here it was done like this for our understanding. Profile connect to the User and User connect to the Profile. Both of them are joind using a reference. There is a one-to-one connection. In Profile side, there it necessarily has a User. But in User side, there it may or may not has a Profile.
--- src/routes/user.mjs
+++ src/routes/user.mjs
@@ -1,13 +1,29 @@
 import { Router } from "express";
 import User from "../model/user.mjs";
+import Profile from "../model/profile.mjs";
 
 const userRouter = Router(); // Execution of userRouter
+
+// As the userSchema object was created in the user.mjs in model folder, this must be worked correctly.
 
 // Get all the data
 userRouter.get("/", async (c, w) => {
   try {
     const users = await User.find();
     return w.status(200).send(users);
+  } catch (error) {
+    console.log(error);
+    return w.status(500).send("internal server error");
+  }
+});
+
+//Get the profile
+userRouter.get("/profile/:userId", async (c, w) => {
+  try {
+    const user = await User.findById(c.params.userId)
+      .populate("profile", "image createdAt")
+      .select(/*["profile", "username"]*/ "profile username name"); // Profile is taken with the user. If it is correct, correct profile comes because it was referenced. To get the profile, there it is used populate('profile'). We can write select(). [] means the argument in the select(). There arguments inside select() can be written as string and without an array.
+    w.status(200).send(user); // This can be sent as json by using json().
   } catch (error) {
     console.log(error);
     return w.status(500).send("internal server error");
@@ -38,5 +54,38 @@ userRouter.post("/", async (c, w) => {
 });
 
 // We have to bring the model to here.
+
+// Update the profile
+userRouter.put("/profile/:userId", async (c, w) => {
+  // When updating the profile, we have to put data in here.
+  // Only image data we have to take in here.
+  const /*data*/ { image } = c.body;
+
+  try {
+    // This is the first way of updating
+    // First we have to find the user.
+    // const user = await User.findById(c.params.userId); // user must be come from this. user is got from userId.
+    // Creation must be done in profile side. Update must be done in user's side.
+    // const profile = await Profile.create({ user: /*user._id*/ c.params.userId, image }); // There it has to put the data inside the {}. The data are passed as an object. This is created from profile's side. User is connected through id when the profile is being created.
+    // After that in the user, this profile must be set and updated.
+    // Then the user is connected to the profile by user._id and it has been made from create().
+
+    // This is the second way of updating.
+    const profile = await Profile.create({ user: c.params.userId, image }); //This creates the profile. After the profile was created, then I get the profile id. userId is already known as it comes from the end-point.
+    const user = await User.findByIdAndUpdate(c.params.userId, {
+      profile: profile._id,
+    }); // {} for object so the data inside curly brackets are object data.
+    // user.profile /* profile is existed as optional */ = profile._id; // This is the id when the profile is created.
+    // await user.save();
+
+    console.log(profile);
+    console.log(user);
+
+    w.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    return w.status(500).send("internal server error");
+  }
+});
 
 export default userRouter;
